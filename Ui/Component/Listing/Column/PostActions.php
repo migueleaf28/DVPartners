fefaa024eab9edb25fb7c46b531b71b07d89cc3f
Miguel Abache <e_meflorez@stefanini.com>
2025-09-26 04:40:19 -0500

    fix: Resolve loading, saving, and DI issues

--- Ui/Component/Listing/Column/PostActions.php
+++ Ui/Component/Listing/Column/PostActions.php
@@ -3,54 +3,76 @@
  * Copyright ©  All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
 
 namespace DVPartners\Blog\Ui\Component\Listing\Column;
 
-use Magento\Ui\Component\Listing\Columns\Column;
-use Magento\Framework\UrlInterface;
-
-class PostActions extends Column
+class PostActions extends \Magento\Ui\Component\Listing\Columns\Column
 {
-    /** @var UrlInterface */
+
+    const URL_PATH_EDIT = 'dvpartners_blog/post/edit';
+    const URL_PATH_DELETE = 'dvpartners_blog/post/delete';
+    const URL_PATH_DETAILS = 'dvpartners_blog/post/details';
     protected $urlBuilder;
 
+    /**
+     * @param \Magento\Framework\View\Element\UiComponent\ContextInterface $context
+     * @param \Magento\Framework\View\Element\UiComponentFactory $uiComponentFactory
+     * @param \Magento\Framework\UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
     public function __construct(
         \Magento\Framework\View\Element\UiComponent\ContextInterface $context,
         \Magento\Framework\View\Element\UiComponentFactory $uiComponentFactory,
-        UrlInterface $urlBuilder,
+        \Magento\Framework\UrlInterface $urlBuilder,
         array $components = [],
         array $data = []
     ) {
-        parent::__construct($context, $uiComponentFactory, $components, $data);
         $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
     }
 
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
     public function prepareDataSource(array $dataSource)
     {
-        if (!isset($dataSource['data']['items'])) {
-            return $dataSource;
-        }
-
-        foreach ($dataSource['data']['items'] as & $item) {
-            if (!isset($item['post_id'])) {
-                continue;
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                if (isset($item['post_id'])) {
+                    $item[$this->getData('name')] = [
+                        'edit' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                static::URL_PATH_EDIT,
+                                [
+                                    'post_id' => $item['post_id']
+                                ]
+                            ),
+                            'label' => __('Edit')
+                        ],
+                        'delete' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                static::URL_PATH_DELETE,
+                                [
+                                    'post_id' => $item['post_id']
+                                ]
+                            ),
+                            'label' => __('Delete'),
+                            'confirm' => [
+                                'title' => __('Delete "${ $.$data.title }"'),
+                                'message' => __('Are you sure you wan\'t to delete a "${ $.$data.title }" record?')
+                            ]
+                        ]
+                    ];
+                }
             }
-            $item[$this->getData('name')] = [
-                'edit' => [
-                    'href' => $this->urlBuilder->getUrl('dvpartners_blog/post/edit', ['post_id' => $item['post_id']]),
-                    'label' => __('Edit')
-                ],
-                'delete' => [
-                    'href' => $this->urlBuilder->getUrl('dvpartners_blog/post/delete', ['post_id' => $item['post_id']]),
-                    'label' => __('Delete'),
-                    'confirm' => [
-                        'title' => __('Delete "%1"', $item['post_title'] ?? ''),
-                        'message' => __('Are you sure you want to delete this post?')
-                    ]
-                ]
-            ];
         }
-
+        
         return $dataSource;
     }
 }
+
